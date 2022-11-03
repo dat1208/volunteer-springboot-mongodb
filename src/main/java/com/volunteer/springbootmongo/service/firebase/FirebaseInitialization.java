61972840b389c8451a2a3bcede8830799094ab41
dat1208 <86953496+dat1208@users.noreply.github.com>
2022-11-03 23:04:03 +0700

    update user service

---
+++ src/main/java/com/volunteer/springbootmongo/service/firebase/FirebaseInitialization.java
@@ -0,0 +1,12 @@
+package com.volunteer.springbootmongo.service.firebase;
+
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+
+@Service
+public class FirebaseInitialization {
+    public void initialization(){
+
+    }
+}
