ff2c02c8d0358731c22f8023b0ba684a0dec258f
dat1208 <86953496+dat1208@users.noreply.github.com>
2022-12-16 00:43:50 +0700

    getpost api updated

--- src/main/java/com/volunteer/springbootmongo/service/firebase/post/PostService.java
+++ src/main/java/com/volunteer/springbootmongo/service/firebase/post/PostService.java
@@ -41,13 +41,16 @@ public class PostService {
     public ResponseObject savePost(Post post, MultipartFile file, HttpServletRequest request) throws Exception {
       Firestore dbFileStore = FirestoreClient.getFirestore();
       ApiFuture<DocumentReference> collectionApiFuture = dbFileStore.collection(COLLECTION_NAME_POST).add(post);
-
+      String username = jwtUserDetailsService.getUsernameByToken(request);
       String id = collectionApiFuture.get().getId();
       String url = uploadService.uploadPostImage(file,id);
           post.setMainimage(url);
           post.setId(id);
           Long dateTime = System.currentTimeMillis();
           post.setDatecreated(dateTime.toString());
+          post.setOwner(username);
+          post.setAvtOwner(userService.getAvatar(username));
+          post.setNameOwner(userService.getUserByUsername(username).getFirstname()+" "+userService.getUserByUsername(username).getLastname());
       if(post.getType().equals(Post.type.TN) && post.getTotalUsers() == 0)
           return new ResponseObject(HttpStatus.NOT_ACCEPTABLE.toString(), "TN post need totalUsers");
       if(post.getType().equals(Post.type.QG)  && post.getTotalMoney() == 0)
@@ -56,7 +59,7 @@ public class PostService {
           return new ResponseObject(HttpStatus.NOT_ACCEPTABLE.toString(), "HP post need totalMoney");
       collectionApiFuture.get().set(post);
       //Append post to users
-      String username = jwtUserDetailsService.getUsernameByToken(request);
+
       userService.insertPost(id,username);
       //Create collection to save users join TN post
       if(post.getType().equals(Post.type.TN))
@@ -64,6 +67,34 @@ public class PostService {
       return new ResponseObject(HttpStatus.CREATED.toString(),post);
     }
 
+    public ResponseObject join(String idPost, HttpServletRequest request) throws Exception {
+        String username = jwtUserDetailsService.getUsernameByToken(request);
+        //Check user joined
+        Firestore dbFileStore = FirestoreClient.getFirestore();
+        DocumentReference tnpostDoc = dbFileStore.collection(COLLECTION_NAME_TNPOST).document(idPost);
+        List<String> listUsers = null;
+        try {
+            listUsers = tnpostDoc.get().get().toObject(TNPost.class).getListUsers();
+        } catch (Exception ex) {
+        }
+        if(listUsers != null)
+        {
+            for (String un :
+                    listUsers) {
+                if (un.equals(username))
+                    return new ResponseObject(HttpStatus.CONFLICT.toString(), "users_joined");
+            }
+            listUsers.add(username);
+            ApiFuture<WriteResult> future = tnpostDoc.update("listUsers", listUsers);
+        }
+        else if(listUsers == null){
+            List<String> initListUsers = new ArrayList<>();
+            initListUsers.add(username);
+            ApiFuture<WriteResult> future = tnpostDoc.update("listUsers", initListUsers);
+        }
+
+        return new ResponseObject(HttpStatus.OK.toString(), "successful");
+    }
     public ResponseObject getPostDetail(String name) throws ExecutionException, InterruptedException {
         Firestore dbFileStore = FirestoreClient.getFirestore();
         DocumentReference documentReference = dbFileStore.collection(COLLECTION_NAME_POST).document(name);
@@ -113,6 +144,45 @@ public class PostService {
         ApiFuture<WriteResult> collectionApiFuture = dbFileStore.collection(COLLECTION_NAME_TNPOST).document(id).create(tnPost);
 
     }
+
+
+    public int getCurrentUsers(String idPost) throws ExecutionException, InterruptedException {
+        Firestore dbFileStore = FirestoreClient.getFirestore();
+        DocumentReference tnpostDoc = dbFileStore.collection(COLLECTION_NAME_TNPOST).document(idPost);
+        List<String> listUsers = new ArrayList<>();
+        try {
+            listUsers = tnpostDoc.get().get().toObject(TNPost.class).getListUsers();
+        } catch(Exception ex) {
+
+        }
+
+        if (listUsers == null)
+            return 0;
+        return (int) listUsers.stream().count();
+    }
+
+    public List<String> getAvtCurrentUsers(String idPost){
+        Firestore dbFileStore = FirestoreClient.getFirestore();
+        DocumentReference tnpostDoc = dbFileStore.collection(COLLECTION_NAME_TNPOST).document(idPost);
+        List<String> listUsers = new ArrayList<>();
+        List<String> listAvt = new ArrayList<>();
+        try {
+            listUsers = tnpostDoc.get().get().toObject(TNPost.class).getListUsers();
+        } catch(Exception ex) {
+
+        }
+
+        if (listUsers == null)
+            return listAvt;
+
+        for (String user:
+             listUsers) {
+            listAvt.add(userService.getAvatar(user));
+            if(listAvt.stream().count() >= 3)
+                break;
+        }
+        return listAvt;
+    }
     public List<Post> getAll() throws ExecutionException, InterruptedException {
         Firestore dbFileStore = FirestoreClient.getFirestore();
         CollectionReference posts = dbFileStore.collection(COLLECTION_NAME_POST);
@@ -120,6 +190,10 @@ public class PostService {
         for (Post post:listPost) {
             if(post.getDatecreated() != null)
             post.setTimeago(calTimeAgo(Long.valueOf((post.getDatecreated()))));
+            if(post.getType().equals(Post.type.TN)){
+                post.setCurrentUsers(getCurrentUsers(post.getId()));
+                post.setAvtCurrentUsers(getAvtCurrentUsers(post.getId()));
+            }
         }
         return  listPost;
     }
